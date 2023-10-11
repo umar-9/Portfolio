2cc14eb880691d69718a8ddac7a19eceee987a03
umar-9 <umarsultonov21@gmail.com>
2023-10-11 22:42:07 +0100

    Setup

---
+++ script.js
@@ -0,0 +1,44 @@
+setInterval(texter, 4000);
+var txt = "";
+var i = 0;
+
+function texter() {
+    i = 0;
+    var x = document.getElementById("text_changer");
+    const disciplines = ["Aspiring Engineer", "Aspiring Mechanical Engineer", "Aspiring Electronic Engineer", "Aspiring Software Engineer", "Aspiring Aerospace Engineer"];
+    txt = disciplines[(disciplines.indexOf(x.textContent) + 1) % 5];
+    x.innerHTML = "";
+    typewriter();
+}
+window.onload = texter;
+
+function typewriter() {
+    if (i < txt.length) {
+        document.getElementById("text_changer").innerHTML += txt.charAt(i);
+        i++;
+        setTimeout(typewriter, 50);
+    }
+}
+
+function reveal() {
+    var reveals = document.querySelectorAll(".reveal");
+
+    for (var i = 0; i < reveals.length; i++) {
+        var windowHeight = window.innerHeight;
+        var elementTop = reveals[i].getBoundingClientRect().top;
+        var elementVisible = 150;
+
+        if (elementTop < windowHeight - elementVisible) {
+            reveals[i].classList.add("active");
+        } else {
+            reveals[i].classList.remove("active");
+        }
+    }
+}
+
+window.addEventListener("scroll", reveal);
+
+
+function form_processor() {
+    alert("Submitted successfully");
+}
